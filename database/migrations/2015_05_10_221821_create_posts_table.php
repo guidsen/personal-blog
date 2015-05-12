85e40d93cb0d1aa1bbf6d1d2cbb9cf3ca677f759
Guido Schmitz <guisch95@gmail.com>
2015-05-12 12:54:50 +0200

    Added field to migration

--- database/migrations/2015_05_10_221821_create_posts_table.php
+++ database/migrations/2015_05_10_221821_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration {
 		Schema::create('posts', function(Blueprint $table)
 		{
 			$table->increments('id');
+            $table->integer('disqus_id');
             $table->string('title');
             $table->text('body');
             $table->integer('published')->default(0);
